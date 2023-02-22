4961b223324ef47f8f16fa03296d5660173c9a34
Nishant <nishantraj204@gmail.com>
2023-02-22 14:10:12 +0530

    abhishek

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+	
+	private EmployeeService employeeService;
+	
+	public EmployeeController(EmployeeService employeeService) {
+		super();
+		this.employeeService=employeeService;
+	}
+	
+//	@GetMapping("/addEmployee")
+//	public ResponseEntity<Employee>addEmployee(@RequestBody EmployeeService  employee){
+//		return new ResponseEntity<Employee>(employeeService.addEmployee(employee) , HttpStatus.CREATED );
+//	}
+	
+}
